63298b21fb95defb9810770a5b711757f12eb712
nuke7 <gombos.marton@icloud.com>
2021-04-06 01:25:03 +0200

    started o consume and display data from the api

--- src/components/Diagram.jsx
+++ src/components/Diagram.jsx
@@ -1,24 +1,54 @@
+/* eslint-disable no-unused-vars */
 import { Line } from "react-chartjs-2";
+import React, { useState, useEffect } from "react";
 
 export const Diagram = () => {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState({});
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=VQXHRFJAACVTHD2M`
+    );
+    if (response) {
+      const resp = await response.json();
+      console.log(Object.keys(resp["Time Series (Daily)"]).slice(0, 30));
+      setData(resp);
+    } else {
+      setData(null);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  let openValues = Object.values(data["Time Series (Daily)"])
+    .slice(0, 10)
+    .map((val) => val["1. open"]);
+
   return (
     <div>
-      <Line
-        data={{
-          labels: ["1", "2", "3", "4", "5", "6"],
-          datasets: [
-            {
-              label: "# of Votes",
-              data: [12, 19, 3, 5, 2, 3],
-              fill: false,
-              backgroundColor: "rgb(255, 99, 132)",
-              borderColor: "rgba(255, 99, 132, 0.2)",
-            },
-          ],
-        }}
-        height={400}
-        width={600}
-      />
+      {data !== null ? (
+        <Line
+          data={{
+            labels: Object.keys(data["Time Series (Daily)"]).slice(0, 10),
+            datasets: [
+              {
+                label: "Opening Price",
+                data: openValues,
+                fill: false,
+                backgroundColor: "rgb(255, 99, 132)",
+                borderColor: "rgba(255, 99, 132, 0.2)",
+              },
+            ],
+          }}
+          height={400}
+          width={600}
+        />
+      ) : (
+        "No data yet"
+      )}
     </div>
   );
 };
